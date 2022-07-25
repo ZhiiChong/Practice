1ea60222bca774a911c7472bedb8dd5911078056
ZhiiChong <zhiichong@gmail.com>
2022-07-25 19:03:17 +0800

    组件通信

---
+++ vue/05/app1/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>Home - <button @click="fnHome">按钮</button></div>
+    <Hello ref="helloRef"/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hello
+  },
+  methods: {
+    fnHome() {
+      console.log("fnHome");
+      // console.log(this.$refs.helloRef.fnHello());
+      console.log(this.$children);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/05/app1/src/components/Hello.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <div>Hello <World /></div>
+  </div>
+</template>
+
+<script>
+import World from "./World.vue"
+
+export default {
+  name: "HelloCom",
+  components: { World },
+
+  methods: {
+    fnHello() {
+        console.log('fnHello');
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
